44e6bffe24223e71ffc5ef066bddbd5765ac3bce
ZL <ray040406@gmail.com>
2024-11-26 20:34:27 +1300

    resey

--- fullstack/src/App.js
+++ fullstack/src/App.js
@@ -1,11 +1,5 @@
-import { useState } from "react";
-
-const App = (props) => {
-  const [counter, setCounter] = useState(0);
-
-  setTimeout(() => setCounter(counter + 1), 1000);
-
-  return <div>{counter}</div>;
+const App = () => {
+  return <div></div>;
 };
 
 export default App;
