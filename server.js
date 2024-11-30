f8ebb129f4e3d96e11ca9358fdd9959618aab122
ZL <ray040406@gmail.com>
2024-11-30 17:39:04 +1300

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const app = express;
+const app = express();
 
 const jwt = require("jsonwebtoken");
 app.use(express.json());
@@ -19,7 +19,9 @@ app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
   const acessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken: accessToken });
+  res.json({ accessToken: acessToken });
 });
 
-app.listen(3001);
+app.listen(3001, () => {
+  console.log("server running on port 3001");
+});
