ec6ce556ed78f000f1c222eca107748b9ae673aa
ZL <ray040406@gmail.com>
2024-11-30 17:45:21 +1300

    use middleware

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const posts = [
   { username: "as", title: "post 2" },
 ];
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.username));
 });
 
 app.post("/login", (req, res) => {
@@ -21,6 +21,17 @@ app.post("/login", (req, res) => {
   const acessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
   res.json({ accessToken: acessToken });
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3001, () => {
   console.log("server running on port 3001");
