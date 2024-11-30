5f10974e1d642c2e74d3c273ff54dad1502d2e51
ZL <ray040406@gmail.com>
2024-11-30 17:28:39 +1300

    token

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express;
 
+const jwt = require("jsonwebtoken");
+app.use(express.json());
+
 const posts = [
   { username: "zl", title: "post 1" },
-  { username: "zl", title: "post 2" },
+  { username: "as", title: "post 2" },
 ];
 
-app.length("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.post("/login", (req, res) => {
+  // authenticate user
+  const username = req.body.username;
+  const user = { name: username };
+  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+});
+
+app.listen(3001);
