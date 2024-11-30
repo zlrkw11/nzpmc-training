0021fbd28ca76f9c1322672c3aef1f950e113bbf
ZL <ray040406@gmail.com>
2024-11-30 17:20:16 +1300

    setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express;
+
+const posts = [
+  { username: "zl", title: "post 1" },
+  { username: "zl", title: "post 2" },
+];
+
+app.length("/posts", (req, res) => {});
